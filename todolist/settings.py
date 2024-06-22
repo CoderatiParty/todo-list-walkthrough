046fd85767a8f85495a2ee841d3ab1c764102c90
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-06-22 15:01:09 +0000

    Allowed hosts update

--- todolist/settings.py
+++ todolist/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['rich-todo-app-0c7586c727c9.herokuapp.com/']
 
 # Application definition
 
